5406c5f54100ecdcecb1ecd31dec5ea1c3966e83
coderILKIN <99831207+coderILKIN@users.noreply.github.com>
2022-05-04 13:25:18 +0400

    Add files via upload

---
+++ css and javascript tasks/all tasks javascript/assets/js/script.js
@@ -0,0 +1,89 @@
+
+/*
+----1ci task---
+
+function reverseWord (sentence) {
+    return sentence.split(' ').map(function(word) {
+      return word.split('').reverse().join('');
+    }).join(' ');
+  }
+  
+  console.log(reverseWord("hello world"));
+
+  */
+
+
+
+ /*
+ ----4ci task----
+
+
+  let username = prompt('Enter Username');
+  let password = prompt('enter password !');
+
+  if(username == 'username1' && password == 'password1'){
+    console.log('Correct');
+  }else{
+     console.log('Incorrect');
+  }
+  */
+
+
+
+
+/*
+
+-----5ci task-------
+
+  function firsDigit(number) {
+    while (number>=10) {
+      number/=10;
+      return Math.floor(number)
+    }
+  }
+
+  function lastdigit(number) {
+    return Math.floor(number % 10)
+  }
+
+let number = 475;
+
+console.log(firsDigit(number.toString) +lastdigit(number.toString));
+
+*/
+
+
+  
+
+  /*
+------ 6ci task -------
+
+function myFunction(m,n) {
+  return m * n;
+}
+  console.log(myFunction(3,3));
+
+  */
+
+
+
+  
+
+/*
+------7ci task--------
+
+
+ let arr=[30,43,45,67,87,24]
+
+ let result = arr.findIndex((val,i)=>{
+       if(val==45)
+       return i
+ })
+
+ console.log(result);
+
+ */
+
+
+
+ 
